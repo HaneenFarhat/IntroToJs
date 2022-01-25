7bc5805195520f35b6659c7051fd84156553bfe0
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-01-25 17:24:32 +0200

    adding

---
+++ main.js
@@ -0,0 +1,15 @@
+let birthYear =2005
+let currentYear = 2022
+let age = currentYear - birthYear 
+let ageInMonths= age *12
+let firstName = `Haneen`
+let lastName = `Fayeq Farhat`
+let fullName = firstName+" "+lastName 
+console.log(fullName);
+
+
+
+
+let greet =`Hello I'm ${fullName} and I'm ${ageInMonths}  old`
+console.log(greet);
+
