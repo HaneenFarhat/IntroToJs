5d01d821f442ecd0e365a37274fa34541cbf4f47
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-02-03 17:02:38 +0200

    addding

--- main.js
+++ main.js
@@ -20,11 +20,85 @@ console.log(f);
 
 
 
-let count=0
-while(count <=1000){
-    console.log(count);
-    count=count+1
+// let count=0
+// while(count <=1000){
+//     console.log(count);
+//     // count=count+1
+//     // count++
+//     count+=3
 
+// }
+
+// console.log(`finish the counting`);
+
+
+
+// let count=1000
+// let i=0
+// while(count >=1){
+//     console.log(count);
+//     // count=count+1
+//     // count++
+//     // count+=3
+    
+// count /=2
+// i++
+// }
+// const msg =`finish the counting`
+// console.log(i);
+// console.log(msg);
+ 
+
+// let count=1000
+// let i=0
+// while(count >=1){
+//     console.log(count);
+//     // count=count+1
+//     // count++
+//     // count+=3
+    
+// count /=2
+// i++
+// }
+// let msg =`finished the loop in `
+// msg+= i +  ` iterations`
+// console.log(msg);
+
+
+
+
+
+
+
+// let count='*'
+// let i=1
+// while(i<=10){
+//     console.log(count);
+//     // count=count+1
+//     // count++
+//     // count+=3
+    
+// count +='*'
+// i++
+// }
+// let msg =`finished the loop in `
+// msg+= i +  ` iterations`
+// console.log(msg);
+
+
+
+
+
+
+
+
+
+
+const limit=10
+let count = 1
+let stars="*"
+while(count <=limit){
+    console.log(stars);
+    stars+="*"
+    count++
 }
-
-console.log(`finish the counting`);
